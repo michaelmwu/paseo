938fa05d9fbe810a6301112954f5ba232e71cb54
Michael Wu <michaelmwu@gmail.com>
2026-08-06 11:37:22 +0900

    fix(app): stabilize compact provider usage row

--- packages/app/src/composer/index.tsx
+++ packages/app/src/composer/index.tsx
@@ -2592,7 +2592,7 @@ const styles = StyleSheet.create((theme: Theme) => ({
     justifyContent: "center",
   },
   contextWindowMeterCompactSlot: {
-    alignSelf: "flex-start",
+    alignSelf: "stretch",
   },
   realtimeVoiceButton: {
     width: 28,
